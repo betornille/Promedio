dbcacfdac0dfc1526c06102771f0a617f72d632c
betornille <127353936+betornille@users.noreply.github.com>
2023-03-15 20:05:43 -0600

    Update Promedio.c

--- Promedio.c
+++ Promedio.c
@@ -1,18 +1,18 @@
 //Calcular promedio y definir si aprobo
 #include<stdio.h>
     int main(){
-    float n1, n2, n3, n4, suma, promedio;
+    int n1, n2, n3, n4, suma, promedio;
     printf("\nDa primer calificación: ");
-    scanf("%f", &n1);
+    scanf("%d", &n1);
     printf("\nDa segunda calificación: ");
-    scanf("%f", &n2);
+    scanf("%d", &n2);
     printf("\nDa tercer calificación: ");
-    scanf("%f", &n3);
+    scanf("%d", &n3);
     printf("\nDa cuarta calificación: ");
-    scanf("%f", &n4);
+    scanf("%d", &n4);
     suma = n1 + n2 + n3 + n4;
     promedio = suma/4;
-    printf("Tu calificación es: %.2f\n", promedio);
+    printf("Tu calificación es: %.2d\n", promedio);
     if (promedio>5.99)
     {
     printf("Aprobaste\n");   
